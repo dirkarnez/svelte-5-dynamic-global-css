ef4f77c9a89c61a1e93ccf626bb0243a66162a1b
Dirk Arnez <smalldirkalex@gmail.com>
2025-02-05 01:26:44 +0800

    - add Nested.svelte

---
+++ Nested.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onDestroy } from 'svelte';
+
+</script>
+
+<svelte:head>
+	<style>
+		h1 {
+			color: red;
+		}
+	</style>
+</svelte:head>
+
+<h1>Hello world!</h1>
+
+
+<p>...don't affect this element</p>
