fff96059de6662fd72fe58b34be576fd15565133
Dirk Arnez <smalldirkalex@gmail.com>
2025-02-05 01:26:29 +0800

    - add index.svelte

---
+++ index.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Nested from './Nested.svelte';
+	let count = $state(1);
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button onclick={handleClick}>
+	Count: {count}
+</button>
+
+<h1>123242</h1>
+
+<p>These styles...</p>
+{#if count % 2 == 0}
+	<Nested />
+{/if}
